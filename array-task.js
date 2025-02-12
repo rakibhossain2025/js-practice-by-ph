a1eb2109d8e3817bf185fcb736e10fe69cc657fa
rakibhossain2025 <rakibhossain4230@gmail.com>
2025-02-12 20:59:09 +0600

    all task completed

---
+++ array-task.js
@@ -0,0 +1,26 @@
+//1 st task
+const array1 =['amm', 'jam', 'kathal', 'papay','kola','lachu','horbe','amlokhee']
+console.log(array1[3]);
+array1[2]='jambura'
+console.log(array1)
+
+//2 nd task
+const distination =['sundarban', 'khagrrasory','kuakata']
+distination.push('bedash')
+console.log(distination)
+//3ra task
+const array4 = ['raki','hsa','kotha','bol'];
+console.log(array4.includes('raki'))
+//4th task 
+
+const array5 = ['raki1','hsa2','kotha3','bol4'];
+const array6 = [];
+const array7 = 
+console.log(Array.isArray(array5))
+console.log(Array.isArray(array6))
+console.log(Array.isArray(array7))
+
+//5th task
+const concate1 = ['rakib' ,'shakib', 'kakib', 'nakib' ,'bakib'];
+const concate2 = ['rakib1' ,'shakib2', 'kakib3', 'nakib4' ,'bakib5'];
+console.log(concate1.concat(concate2));
---
+++ array.js
@@ -0,0 +1,21 @@
+const number =[1,2,3,4,5,6,7,8,9,0];
+console.log(number.indexOf(0)); // result dependent in value and output is value ta koi number index a asa
+console.log(number[8]); // result dependent in index
+const five = number[8];
+console.log(five); //3 number index ar valu bar korba
+// aray ends
+const second = ['rakib','akib','takin','takin2','takin3','takin4','takin5'];
+ second.pop()
+ second.shift()
+ second.unshift('baloi to amar balo aga hehehehehhe')
+ console.log(second);
+
+
+ const array3 =[12,2123,12,34,12,4,1234,14]
+ if (array3.includes(121)){
+  console.log('good')
+ }
+ 
+ else{
+  console.log('naai')
+ }
